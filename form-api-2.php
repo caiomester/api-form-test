5a806e9e896922a14ef024e86e5052bcf1aba21e
caiomester <caio.murillo10@gmail.com>
2022-01-18 11:13:14 -0300

    Update form-api-2.php

--- form-api-2.php
+++ form-api-2.php
@@ -6,32 +6,28 @@
 	
 	//Conexão
 	$url = 'https://homolog.dropserv.com.br/api/v2/employer-register';
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_POST, true);
+    	$ch = curl_init();
+    	curl_setopt($ch, CURLOPT_URL, $url);
+    	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    	curl_setopt($ch, CURLOPT_POST, true);
 
-    $data = array(
-        'nome' => $nome,
-        'email' => $email
-    );
+    	$data = array(
+        	'nome' => $nome,
+        	'email' => $email
+    	);
 
 	//Enviar os dados
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-    $output = curl_exec($ch);
+    	curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+    	$output = curl_exec($ch);
 
-	//Exibir Info do Curl
-	//$info = curl_getinfo($ch);
-	//print_r ($info);
+	//Caso haja a conexão exibir detalhes
+	$info = curl_getinfo($ch);
+	print_r ($info);
 
 	//Encerrar conexão
-    curl_close($ch);
+    	curl_close($ch);
 	
 	//Retornar resultados
-	$result 			= 	unserialize($output);
-	$result_status 		= 	$result['status'];
-	$result_mensagem 	= 	$result['mensagemRetorno'];
-
-	echo $result_status . ' : ' . $result_mensagem;
+	var_dump(json_decode($output, true));
 
 ?>
